3abe2838f3c2f84f11c0d1e30409a5f2a0c4a456
cpdf98 <cpdf98@yahoo.com>
2022-03-16 13:45:29 -0400

    Fixed lazygit binding

--- lua/settings/mappings.lua
+++ lua/settings/mappings.lua
@@ -1,7 +1,7 @@
 local map = require('settings.utils').map
 map('n', '<C-n>', ':bnext<CR>', opts)
 map('n', '<C-p>', ':bprevious<CR>', opts)
-map('n', '<C-l', ':LazyGit<CR>', opts)
+map('n', '<C-l>', ':LazyGit<CR>', opts)
 map('n', '<C-i>', ':w<CR>', opts)
 map('n', '<C-h>', ':bd<CR>', opts)
 map('n', '<F7>', ':so ~/.config/nvim/init.lua<CR>', opts)
