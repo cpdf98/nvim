107bca9b326743c68f0220f334d9918b77fa28a7
cpdf98 <cpdf98@yahoo.com>
2022-03-19 04:15:59 -0400

    Moved signify config file to lua

--- lua/config/signify.lua
+++ lua/config/signify.lua
@@ -1,9 +1,13 @@
-set updatetime=100
-let g:signify_sign_add               = '▍'
-let g:signify_sign_delete            = '▍'
-let g:signify_sign_delete_first_line = '▍'
-let g:signify_sign_change            = '▍'
+vim.cmd('set updatetime=100')
+vim.g.signify_sign_add               = '▍'
+vim.g.signify_sign_delete            = '▍'
+vim.g.signify_sign_delete_first_line = '▍'
+vim.g.signify_sign_change            = '▍'
+
+vim.cmd([[
 highlight clear SignColumn
 highlight SignifySignAdd    ctermfg=green  guifg=#00ff00 cterm=NONE gui=NONE
 highlight SignifySignDelete ctermfg=red    guifg=#ff0000 cterm=NONE gui=NONE
 highlight SignifySignChange ctermfg=blue guifg=#3630E1 cterm=NONE gui=NONE
+]])
+
